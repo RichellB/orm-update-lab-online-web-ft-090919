4587f0e2cf58d93480ce849e0316a198aaf38bf7
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-16 08:26:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
         INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
-      DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      DB[:conn].execute(sql, name, grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
     end 
