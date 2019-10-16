f980b6e92a3dff13ea05d2eb805c781bf426ec9b
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-16 08:13:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,5 +32,13 @@ class Student
   def save 
     if self.id 
       self.update
+    else 
+      sql = <<--SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end 
+  end
 
 end
