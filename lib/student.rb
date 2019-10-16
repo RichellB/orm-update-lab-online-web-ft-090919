da5d4384013dfd38e2aa930438c3a5e19673d0a3
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-16 08:21:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,5 +54,15 @@ class Student
     student.grade = row[2]
     student 
   end
-
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end 
+  
+  def update 
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end 
 end
